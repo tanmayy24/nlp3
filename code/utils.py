e267523160efea8f70a5af29a12e079d1ece1444
tanmayy24 <tk3309nyu.edu>
2024-10-27 11:28:48 -0400

    Setupnlp3

--- code/utils.py
+++ code/utils.py
@@ -8,6 +8,7 @@ from transformers import get_scheduler
 import torch
 from tqdm.auto import tqdm
 import evaluate
+import re
 import random
 import argparse
 from nltk.corpus import wordnet
@@ -34,18 +35,54 @@ def example_transform(example):
 # You can randomly select each word with some fixed probability to replace by a synonym.
 
 
-def custom_transform(example):
+# Define a dictionary of common contractions and their expanded forms
+contractions = {
+    "do not": "don't", "I am": "I'm", "you are": "you're", "we are": "we're", "they are": "they're",
+    "is not": "isn't", "are not": "aren't", "cannot": "can't", "will not": "won't", "did not": "didn't",
+    "would not": "wouldn't", "should not": "shouldn't", "could not": "couldn't", "have not": "haven't",
+    "has not": "hasn't", "had not": "hadn't", "it is": "it's", "that is": "that's", "there is": "there's",
+    "what is": "what's", "who is": "who's", "let us": "let's", "we will": "we'll", "you will": "you'll",
+    "they will": "they'll", "he will": "he'll", "she will": "she'll", "I will": "I'll", "it will": "it'll",
+    "I would": "I'd", "you would": "you'd", "he would": "he'd", "she would": "she'd", "we would": "we'd",
+    "they would": "they'd", "there are": "there're", "I have": "I've", "you've": "you have", "we've": "we have",
+    "they've": "they have", "that'd": "that would", "who'd": "who would", "where's": "where is",
+    "how's": "how is", "when's": "when is", "why's": "why is"
+}
+
+# Reverse mapping for decontractions
+decontractions = {v: k for k, v in contractions.items()}
+
+def contractions_transform(text):
+    # Replace phrases with their contracted forms
+    for phrase, contraction in contractions.items():
+        text = re.sub(r'\b' + re.escape(phrase) + r'\b', contraction, text, flags=re.IGNORECASE)
+    return text
+
+def decontractions_transform(text):
+    # Replace contractions with their expanded forms
+    for contraction, phrase in decontractions.items():
+        text = re.sub(r'\b' + re.escape(contraction) + r'\b', phrase, text, flags=re.IGNORECASE)
+    return text
+
+def custom_transform(example, contraction_prob=0.5):
     ################################
-    ##### YOUR CODE BEGINGS HERE ###
+    ##### YOUR CODE BEGINS HERE ###
 
-    # Design and implement the transformation as mentioned in pdf
-    # You are free to implement any transformation but the comments at the top roughly describe
-    # how you could implement two of them --- synonym replacement and typos.
-
-    # You should update example["text"] using your transformation
-
-    raise NotImplementedError
+    # Randomly decide whether to apply contraction or decontraction based on the given probability
+    if random.random() < contraction_prob:
+        example["text"] = contractions_transform(example["text"])
+    else:
+        example["text"] = decontractions_transform(example["text"])
 
     ##### YOUR CODE ENDS HERE ######
 
     return example
+
+
+# Example usage
+example_text = {"text": "I am not sure if you are going to like it."}
+random.seed(42)  # For reproducibility
+print(custom_transform(example_text))
+
+example_text_2 = {"text": "I'm not sure if you're going to like it."}
+print(custom_transform(example_text_2))
